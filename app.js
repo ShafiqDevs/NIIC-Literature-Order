161ae2e645fefa39fd543f8269018f6534dcd129
Shafiq <shafiq.belaroussi@gmail.com>
2022-06-28 20:02:46 +0100

    cleaned up comments but ignore this.

--- app.js
+++ app.js
@@ -22,34 +22,7 @@ const myRoutes = {
 
 
 
-// NOTE: objects below will be represented as db documents in mongoose at a later commit
-// Value	Weight	Length	Width	Height	delivery-cost
 
-const polishQuran = {
-
-    vlaue: 60,
-    weight: 10,
-    length: 30,
-    width: 24,
-    height: 25,
-    deliveryCost: 8.2
-};
-
-const englishQuran = {
-
-    vlaue: 70,
-    weight: 15,
-    length: 35,
-    width: 25,
-    height: 25,
-    deliveryCost: 8.2
-};
-
-// NOTE: this collection will be added later as a db with mongoose
-// const quranCollection = {
-//     polishQuran,
-//     englishQuran
-// };
 
 
 
