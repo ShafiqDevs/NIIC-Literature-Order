5a0c7d071ba1f153aa247cc011082c370c0c084b
Shafiq <shafiq.belaroussi@gmail.com>
2022-07-17 22:46:53 +0100

    prepare for publish Test version on Vercel

--- components/BrandBar.jsx
+++ components/BrandBar.jsx
@@ -1,15 +1,20 @@
 import { useState } from "react";
 import ShoppingCart from "./ShoppingCartIcon";
+import Link from "next/link";
 
 export default function BrandBar(props) {
   return (
     <div>
       <nav className="navbar bg-dark">
         <div className="container">
-          <a className="navbar-brand" href="/">
-            <img src="../images/niic.png" width={150} height={150} />
-          </a>
-          <ShoppingCart onClick={props.cartIconClick} cartItemCount={props.cartItemCount}
+          <Link href="/">
+            <a className="navbar-brand">
+              <img src="../images/niic.png" width={150} height={150} />
+            </a>
+          </Link>
+          <ShoppingCart
+            onClick={props.cartIconClick}
+            cartItemCount={props.cartItemCount}
           />
         </div>
       </nav>
