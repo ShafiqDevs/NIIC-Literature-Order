d4c57649d78e632ffd8ec275a8341deba8bc01a9
Shafiq <shafiq.belaroussi@gmail.com>
2022-07-18 16:00:04 +0100

    fix api

--- pages/index.jsx
+++ pages/index.jsx
@@ -122,7 +122,7 @@ export const getServerSideProps = async (context) => {
       return {
         redirect: {
           permanent: false,
-          destination: "http://https://niic-online-shope.vercel.app/",
+          destination: "https://niic-online-shope.vercel.app/",
         },
         props: {},
       };
